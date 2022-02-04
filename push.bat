dbce17982e831100a89eaa5df747d522dad9a97e
jose <jose_llano_95@hotmail.com>
2022-02-04 20:20:20 -0300

    update

--- push.bat
+++ push.bat
@@ -1,3 +1,8 @@
+cd "C:\Users\jose_\AppData\LocalLow\IronGate\Valheim\worlds" 
+copy ELCDP.db "F:\worlds github"
+copy ELCDP.fwl "F:\worlds github"
+f:
+cd "F:\worlds github"
 git status
 git add .
 git commit -m "update"
